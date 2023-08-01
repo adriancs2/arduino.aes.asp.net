5399b570e3b7df882e7aa169eca8d0bcc8d2aa0d
adriancs <avoomj@yahoo.com>
2023-08-01 12:15:34 +0800

    Update aes-demo-arduino-only.ino

--- arduino/aes-demo-arduino-only/aes-demo-arduino-only.ino
+++ arduino/aes-demo-arduino-only/aes-demo-arduino-only.ino
@@ -1,16 +1,17 @@
-// The AES Encryption Library
+// import AES encryption library
 #include "AESLib.h"
+
+// import base64 conversion library
 #include "arduino_base64.hpp"
 
 // declare a global AESLib object
 AESLib aesLib;
 
 void setup() {
-    // put your setup code here, to run once:
-
-    // 
+    // begin the bit per second communication speed
+    // between Arduino and computer for serial port monitoring
     Serial.begin(115200);
-
+    
     String encryptedText = "";
     String decryptedText = "";
     String url = "";
@@ -62,6 +63,12 @@ String encrypt(String inputText) {
     // declare an empty byte array (a memory storage)
     byte bytesEncrypted[outputLength];
 
+    // initializing AES engine
+
+    // Cipher Mode and Key Size are preset in AESLib
+    // Cipher Mode = CBC
+    // Key Size = 128
+
     // declare the KEY and IV
     byte aesKey[] = { 23, 45, 56, 67, 67, 87, 98, 12, 32, 34, 45, 56, 67, 87, 65, 5 };
     byte aesIv[] = { 123, 43, 46, 89, 29, 187, 58, 213, 78, 50, 19, 106, 205, 1, 5, 7 };
@@ -70,6 +77,12 @@ String encrypt(String inputText) {
     aesLib.set_paddingmode((paddingMode)0);
 
     // encrypt the bytes in "bytesInput" and store the output at "bytesEncrypted"
+    // param 1 = the source bytes to be encrypted
+    // param 2 = the length of source bytes
+    // param 3 = the destination of encrypted bytes that will be saved
+    // param 4 = KEY
+    // param 5 = the length of KEY bytes (16)
+    // param 6 = IV
     aesLib.encrypt(bytesInput, bytesInputLength, bytesEncrypted, aesKey, 16, aesIv);
 
     // declare a empty char array
@@ -82,7 +95,7 @@ String encrypt(String inputText) {
     return String(base64EncodedOutput);
 }
 
-// the decription function
+// the decryption function
 String decrypt(String encryptedBase64Text) {
 
     // calculate the original length before it was coded into base64 string
@@ -96,6 +109,12 @@ String decrypt(String encryptedBase64Text) {
     // which is the encryptedBytes
     base64::decode(encryptedBase64Text.c_str(), encryptedBytes);
 
+    // initializing AES engine
+
+    // Cipher Mode and Key Size are preset in AESLib
+    // Cipher Mode = CBC
+    // Key Size = 128
+
     // declare the KEY and IV
     byte aesKey[] = { 23, 45, 56, 67, 67, 87, 98, 12, 32, 34, 45, 56, 67, 87, 65, 5 };
     byte aesIv[] = { 123, 43, 46, 89, 29, 187, 58, 213, 78, 50, 19, 106, 205, 1, 5, 7 };
@@ -104,6 +123,12 @@ String decrypt(String encryptedBase64Text) {
     aesLib.set_paddingmode((paddingMode)0);
 
     // decrypt bytes in "encryptedBytes" and save the output in "decryptedBytes"
+    // param 1 = the source bytes to be decrypted
+    // param 2 = the length of source bytes
+    // param 3 = the destination of decrypted bytes that will be saved
+    // param 4 = KEY
+    // param 5 = the length of KEY bytes (16)
+    // param 6 = IV
     aesLib.decrypt(encryptedBytes, originalBytesLength, decryptedBytes, aesKey, 16, aesIv);
 
     // convert the decrypted bytes into original string
